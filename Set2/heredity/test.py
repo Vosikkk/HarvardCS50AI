50c2711c57dfda3263efbbc1bcfdbb3dbe287631
Vosik <vosikvos@gmail.com>
2024-12-24 00:31:02 +0200

    F!! - finish joint_probability function for Set2/heredity

---
+++ Set2/heredity/test.py
@@ -0,0 +1,13 @@
+
+import heredity 
+
+people = {
+  'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
+  'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
+  'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
+}
+
+res = heredity.joint_probability(people, {"Harry"}, {"James"}, {"James"})
+
+
+print(res)
